e3f262d0e04de855276dbdb8613a7f757ddba240
Debendra Barik <debendra.barik@cardinaltsinc.com>
2017-06-14 21:30:38 +0530

    changed to by pass splash screen

--- android/r2/src/pages/pages.ts
+++ android/r2/src/pages/pages.ts
@@ -1,7 +1,5 @@
-import { ListMasterPage } from './list-master/list-master';
+
 import { SearchPage } from './search/search';
-import { SettingsPage } from './settings/settings';
-import { TabsPage } from './tabs/tabs';
 import { WelcomePage } from './welcome/welcome';
 
 // The page the user lands on after opening the app and without a session
@@ -9,9 +7,9 @@ export const FirstRunPage = WelcomePage;
 
 // The main page the user will see as they use the app over a long period of time.
 // Change this if not using tabs
-export const MainPage = TabsPage;
+export const MainPage = SearchPage;
 
 // The initial root pages for our tabs (remove if not using tabs)
-export const Tab1Root = ListMasterPage;
-export const Tab2Root = SearchPage;
-export const Tab3Root = SettingsPage;
+//export const Tab1Root = ListMasterPage;
+//export const Tab2Root = SearchPage;
+//export const Tab3Root = SettingsPage;
