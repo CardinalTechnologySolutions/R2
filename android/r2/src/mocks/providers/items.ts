07b58f81eb4a8e90eca591623b7458aa602af35a
AkhileshPal <akhilesh.pal.cardinalts@gmail.com>
2017-06-19 16:21:35 +0530

    src updated

--- android/r2/src/mocks/providers/items.ts
+++ android/r2/src/mocks/providers/items.ts
@@ -18,10 +18,10 @@ export class Items {
   constructor(public http: Http) {
     let items = [
       {
-        "name": "Lucknow",
-		"rating": "*****",
-        "profilePic": "assets/img/speakers/bear.jpg",
-        "about": "Lucknow, a large city in northern India, is the capital of the state of Uttar Pradesh. Toward its center is Rumi Darwaza, a Mughal gateway. Nearby, the 18th-century Bara Imambara shrine has a huge arched hall. Upstairs, Bhool Bhulaiya is a maze of narrow tunnels with city views from its upper balconies. Close by, the grand Victorian Husainabad Clock Tower was built as a victory column in 1881."
+        "name": "New York City",
+		"special": "Did you know: New York is the fourth-most-populous city in the Americas (8,550,405).",
+        "profilePic": "assets/img/speakers/newyork.jpg",
+        "about": "New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park. Broadway theater is staged in neon-lit Times Square."
       },
       {
         "name": "Charlie Cheetah",
