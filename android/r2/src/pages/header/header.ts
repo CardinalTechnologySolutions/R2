fcc55b96df9abeb127e2cd56ff8e9b4ac6dbee51
Debendra Barik <debendra.barik@cardinaltsinc.com>
2017-08-07 17:53:48 +0530

    added validation logic on user login.

--- android/r2/src/pages/header/header.ts
+++ android/r2/src/pages/header/header.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { NavController, NavParams, Config} from 'ionic-angular';
-import { MainPage } from '../../pages/pages';
+import { MainPage } from '../pages';
+import { LoginPage } from '../login/login';
 
 
 @Component({
@@ -19,9 +20,21 @@ export class HeaderPage {
   ionViewDidLoad() {
   }
    goToHome() {	
+	var view = this.navCtrl.getActive();
+	if(view.component.name === 'SearchPage'){
+		return;
+	}
 	var token = this.config.get('r3_access_token');
 	if(token != null && token != undefined){
 		this.navCtrl.push(MainPage);
 	}    
+  }
+  logout() {	
+	var view = this.navCtrl.getActive();
+	if(view.component.name === 'LoginPage'){
+		return;
+	}
+	this.config.set('r3_access_token', null);
+	this.navCtrl.push(LoginPage);
   }
 }
